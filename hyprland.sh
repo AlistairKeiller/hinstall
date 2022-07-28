405b2772b25089f1b2a1ca3f028bc74ab3f78602
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2022-07-28 14:17:00 -0700

    Update hyprland.sh

--- hyprland.sh
+++ hyprland.sh
@@ -12,9 +12,13 @@ sddm \
 alacritty zsh oh-my-zsh-git fzf zsh-autosuggestions zsh-syntax-highlighting zsh-theme-powerlevel10k-git ttf-meslo-nerd-font-powerlevel10k
 
 systemctl enable sddm
+chsh -s /usr/bin/zsh
 
 cp config/.zshrc ~
-mkdir -p ~/.config/alacritty/
+
+mkdir ~/.config/alacritty
 cp config/alacritty.yml ~/.config/alacritty
 
-chsh -s /usr/bin/zsh
+mkdir ~/.config/hypr
+cp config/hyprland.yml ~/.config/hypr
+
