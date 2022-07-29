86b525df21a8367890b70726cf32a9941a3bb132
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2022-07-29 11:26:18 -0700

    Update yay.sh

--- helper/yay.sh
+++ helper/yay.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-git config --global user.name 'Alistair Keiller'
-git config --global user.email 'alistair@keiller.net'
-sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 sudo pacman -Syu --noconfirm
 git clone https://aur.archlinux.org/yay
 cd yay
