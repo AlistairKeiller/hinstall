05ab964991ffd403c6e2421cb34563c5fa5126c1
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2022-08-03 15:13:28 -0700

    Update hyprland.sh

--- hyprland.sh
+++ hyprland.sh
@@ -10,7 +10,7 @@ yay -Syu --noconfirm \
   wofi \
   grim slurp \
   sddm sddm-sugar-candy-git \
-  polkit-gnome gnome-keyring \
+  polkit-gnome \
   alacritty zsh oh-my-zsh-git fzf zsh-autosuggestions zsh-syntax-highlighting zsh-theme-powerlevel10k-git ttf-meslo-nerd-font-powerlevel10k \
   firefox \
   webcord \
